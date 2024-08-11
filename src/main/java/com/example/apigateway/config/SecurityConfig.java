6aa3b2efb69d88f3acc77217deafac5b2c1443be
grfs ahwre <michael.d.keyes87@gmail.com>
2024-08-11 18:17:02 -0400

    swagger

--- src/main/java/com/example/apigateway/config/SecurityConfig.java
+++ src/main/java/com/example/apigateway/config/SecurityConfig.java
@@ -5,14 +5,39 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(final HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
-                .build();
-    }
+  private final String[] freeResourceUrls = {"/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/swagger" +
+      "-resources/**", "/aggregate/**"};
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(final HttpSecurity httpSecurity) throws Exception {
+    return httpSecurity.authorizeHttpRequests(authorize -> authorize
+            .requestMatchers(freeResourceUrls).permitAll()
+            .anyRequest().authenticated())
+        .cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource()))
+        .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+        .build();
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    final CorsConfiguration corsConfiguration = new CorsConfiguration();
+    corsConfiguration.setAllowedOrigins(List.of("*"));
+    corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST"));
+    corsConfiguration.setAllowedHeaders(List.of("*"));
+
+    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", corsConfiguration);
+
+    return source;
+  }
 }
